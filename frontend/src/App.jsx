85bfd353949f6dc8f4db17c0b1258750c3f9c789
and3k <andrewjkasten@gmail.com>
2026-03-07 13:15:14 -0700

    BrowserRouter as Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import Login from './pages/Login';
 import Signup from './pages/Signup';
